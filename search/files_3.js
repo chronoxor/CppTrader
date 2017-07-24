d2910a782be75c6401046dfc9bbfe4a1c569c205
Travis CI <travis@travis-ci.org>
2017-07-24 14:15:15 +0000

    Update generated documentation

--- search/files_3.js
+++ search/files_3.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['version_2eh',['version.h',['../template_2version_8h.html',1,'']]],
-  ['version_2eh',['version.h',['../trader_2version_8h.html',1,'']]]
+  ['version_2eh',['version.h',['../template_2version_8h.html',1,'(Global Namespace)'],['../trader_2version_8h.html',1,'(Global Namespace)']]]
 ];
