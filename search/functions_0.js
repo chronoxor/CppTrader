ba499f0876f781a25370e67c2533416f67ee1cb5
AppVeyor <team@appveyor.com>
2017-07-31 12:52:24 -0700

    Update generated documentation

--- search/functions_0.js
+++ search/functions_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['empty',['empty',['../class_cpp_trader_1_1_order_manager.html#a766588137bffd11c1f919a5e4a1dfc14',1,'CppTrader::OrderManager::empty()'],['../class_cpp_trader_1_1_symbol_manager.html#a2c60b9864456c0a4f055739a39602553',1,'CppTrader::SymbolManager::empty()']]]
+  ['empty',['empty',['../class_cpp_trader_1_1_order_manager.html#a312794a8277eceb70bd1f0f9a5cd1f9f',1,'CppTrader::OrderManager::empty()'],['../class_cpp_trader_1_1_symbol_manager.html#a6472c275b8b393b735755c555b883f4e',1,'CppTrader::SymbolManager::empty()']]]
 ];
