9518c1aa819b2212fd301818e261e503d9adf9bc
Travis CI <travis@travis-ci.org>
2017-08-03 15:23:44 +0000

    Update generated documentation

--- search/files_2.js
+++ search/files_2.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['order_5fbook_2eh',['order_book.h',['../order__book_8h.html',1,'']]],
+  ['order_5fmanager_2ecpp',['order_manager.cpp',['../order__manager_8cpp.html',1,'']]],
   ['order_5fmanager_2eh',['order_manager.h',['../order__manager_8h.html',1,'']]],
   ['order_5fmanager_2einl',['order_manager.inl',['../order__manager_8inl.html',1,'']]]
 ];
--- search/files_3.js
+++ search/files_3.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['symbol_5fmanager_2ecpp',['symbol_manager.cpp',['../symbol__manager_8cpp.html',1,'']]],
   ['symbol_5fmanager_2eh',['symbol_manager.h',['../symbol__manager_8h.html',1,'']]],
   ['symbol_5fmanager_2einl',['symbol_manager.inl',['../symbol__manager_8inl.html',1,'']]]
 ];
