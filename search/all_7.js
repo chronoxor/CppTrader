e4c4611cd2c6f5aa086732bb7de6abeb7eb4cef6
Travis CI <travis@travis-ci.org>
2017-08-04 20:10:57 +0000

    Update generated documentation

--- search/all_7.js
+++ search/all_7.js
@@ -11,9 +11,9 @@ var searchData=
   ['iporeleasetime',['IPOReleaseTime',['../struct_cpp_trader_1_1_i_t_c_h_1_1_i_p_o_quoting_message.html#a8951e20ae828e5ddbaf3fcf433266934',1,'CppTrader::ITCH::IPOQuotingMessage']]],
   ['issueclassification',['IssueClassification',['../struct_cpp_trader_1_1_i_t_c_h_1_1_stock_directory_message.html#a7fe3fcce406cdb6cbf65e556a7eb2360',1,'CppTrader::ITCH::StockDirectoryMessage']]],
   ['issuesubtype',['IssueSubType',['../struct_cpp_trader_1_1_i_t_c_h_1_1_stock_directory_message.html#a759c2ed5b1947755466534da03582268',1,'CppTrader::ITCH::StockDirectoryMessage']]],
-  ['itch',['ITCH',['../namespace_i_t_c_h.html',1,'']]],
   ['itch_5fhandler_2ecpp',['itch_handler.cpp',['../itch__handler_8cpp.html',1,'']]],
   ['itch_5fhandler_2eh',['itch_handler.h',['../itch__handler_8h.html',1,'']]],
   ['itch_5fhandler_2einl',['itch_handler.inl',['../itch__handler_8inl.html',1,'']]],
-  ['itchhandler',['ITCHHandler',['../class_cpp_trader_1_1_i_t_c_h_1_1_i_t_c_h_handler.html',1,'CppTrader::ITCH::ITCHHandler'],['../class_cpp_trader_1_1_i_t_c_h_1_1_i_t_c_h_handler.html#abcf8e0c8fab2fe8f9c90cb4db5b0318d',1,'CppTrader::ITCH::ITCHHandler::ITCHHandler()'],['../class_cpp_trader_1_1_i_t_c_h_1_1_i_t_c_h_handler.html#aa53fd288010e9c630b605d04edbde80d',1,'CppTrader::ITCH::ITCHHandler::ITCHHandler(const ITCHHandler &amp;)=delete'],['../class_cpp_trader_1_1_i_t_c_h_1_1_i_t_c_h_handler.html#a859f0f2efd273d0575f983bdc0abc987',1,'CppTrader::ITCH::ITCHHandler::ITCHHandler(ITCHHandler &amp;&amp;) noexcept=default']]]
+  ['itchhandler',['ITCHHandler',['../class_cpp_trader_1_1_i_t_c_h_1_1_i_t_c_h_handler.html',1,'CppTrader::ITCH']]],
+  ['itchhandler',['ITCHHandler',['../class_cpp_trader_1_1_i_t_c_h_1_1_i_t_c_h_handler.html#abcf8e0c8fab2fe8f9c90cb4db5b0318d',1,'CppTrader::ITCH::ITCHHandler::ITCHHandler()'],['../class_cpp_trader_1_1_i_t_c_h_1_1_i_t_c_h_handler.html#aa53fd288010e9c630b605d04edbde80d',1,'CppTrader::ITCH::ITCHHandler::ITCHHandler(const ITCHHandler &amp;)=delete'],['../class_cpp_trader_1_1_i_t_c_h_1_1_i_t_c_h_handler.html#a859f0f2efd273d0575f983bdc0abc987',1,'CppTrader::ITCH::ITCHHandler::ITCHHandler(ITCHHandler &amp;&amp;) noexcept=default']]]
 ];
