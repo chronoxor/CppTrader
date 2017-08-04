60723a4d3f30558a3102098f9bcca202a17a8d91
Travis CI <travis@travis-ci.org>
2017-08-04 10:38:30 +0000

    Update generated documentation

--- search/all_f.js
+++ search/all_f.js
@@ -10,7 +10,8 @@ var searchData=
   ['symbol_5fmanager_2ecpp',['symbol_manager.cpp',['../symbol__manager_8cpp.html',1,'']]],
   ['symbol_5fmanager_2eh',['symbol_manager.h',['../symbol__manager_8h.html',1,'']]],
   ['symbol_5fmanager_2einl',['symbol_manager.inl',['../symbol__manager_8inl.html',1,'']]],
-  ['symbolmanager',['SymbolManager',['../class_cpp_trader_1_1_symbol_manager.html',1,'CppTrader::SymbolManager'],['../class_cpp_trader_1_1_symbol_manager.html#a789c1f36c4856c4d540a8c5ba55e16ae',1,'CppTrader::SymbolManager::SymbolManager()'],['../class_cpp_trader_1_1_symbol_manager.html#ac6823afae113800c7022c10e9ebfa9e8',1,'CppTrader::SymbolManager::SymbolManager(const SymbolManager &amp;)=delete'],['../class_cpp_trader_1_1_symbol_manager.html#ae2577e4e329bc4aa2db6ccebfccdd879',1,'CppTrader::SymbolManager::SymbolManager(SymbolManager &amp;&amp;) noexcept=default']]],
+  ['symbolmanager',['SymbolManager',['../class_cpp_trader_1_1_symbol_manager.html',1,'CppTrader']]],
+  ['symbolmanager',['SymbolManager',['../class_cpp_trader_1_1_symbol_manager.html#a789c1f36c4856c4d540a8c5ba55e16ae',1,'CppTrader::SymbolManager::SymbolManager()'],['../class_cpp_trader_1_1_symbol_manager.html#ac6823afae113800c7022c10e9ebfa9e8',1,'CppTrader::SymbolManager::SymbolManager(const SymbolManager &amp;)=delete'],['../class_cpp_trader_1_1_symbol_manager.html#ae2577e4e329bc4aa2db6ccebfccdd879',1,'CppTrader::SymbolManager::SymbolManager(SymbolManager &amp;&amp;) noexcept=default']]],
   ['symbols',['symbols',['../class_cpp_trader_1_1_market_manager.html#a9273ac04f81b2cd822799e810b771233',1,'CppTrader::MarketManager']]],
   ['systemeventmessage',['SystemEventMessage',['../struct_cpp_trader_1_1_i_t_c_h_1_1_system_event_message.html',1,'CppTrader::ITCH']]]
 ];
--- search/functions_8.js
+++ search/functions_8.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['marketmanager',['MarketManager',['../class_cpp_trader_1_1_market_manager.html#a9aad8b89f8abd1da56cde3f646924851',1,'CppTrader::MarketManager::MarketManager()'],['../class_cpp_trader_1_1_market_manager.html#a4e255cca78f825642f067c9fce70dd70',1,'CppTrader::MarketManager::MarketManager(const MarketManager &amp;)=delete'],['../class_cpp_trader_1_1_market_manager.html#ae933eeef3b4fa6362d9c7a44b99f4f20',1,'CppTrader::MarketManager::MarketManager(MarketManager &amp;&amp;)=default']]]
+  ['marketmanager',['MarketManager',['../class_cpp_trader_1_1_market_manager.html#a9aad8b89f8abd1da56cde3f646924851',1,'CppTrader::MarketManager::MarketManager()'],['../class_cpp_trader_1_1_market_manager.html#a4e255cca78f825642f067c9fce70dd70',1,'CppTrader::MarketManager::MarketManager(const MarketManager &amp;)=delete'],['../class_cpp_trader_1_1_market_manager.html#ae933eeef3b4fa6362d9c7a44b99f4f20',1,'CppTrader::MarketManager::MarketManager(MarketManager &amp;&amp;)=default']]],
+  ['modifyorder',['ModifyOrder',['../class_cpp_trader_1_1_market_manager.html#ac096abe58fe585c07f925787fffdf0ca',1,'CppTrader::MarketManager']]]
 ];
