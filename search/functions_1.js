4452b5f1e1196156d4e04b71d56c6c4fbcfb2a99
Travis CI <travis@travis-ci.org>
2017-08-07 11:16:39 +0000

    Update generated documentation

--- search/functions_1.js
+++ search/functions_1.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['best_5fask',['best_ask',['../class_cpp_trader_1_1_order_book.html#a4009205412a39be60278df619b5c343f',1,'CppTrader::OrderBook']]],
-  ['best_5fbid',['best_bid',['../class_cpp_trader_1_1_order_book.html#ab98eaee2dead01078b4c089fb61438b9',1,'CppTrader::OrderBook']]],
-  ['bids',['bids',['../class_cpp_trader_1_1_order_book.html#a7ad6d2d0797a05b449610a2af112b01c',1,'CppTrader::OrderBook']]]
+  ['best_5fask',['best_ask',['../class_cpp_trader_1_1_order_book.html#aa0b7847d5d53f5adf938fcdc02b312d6',1,'CppTrader::OrderBook']]],
+  ['best_5fbid',['best_bid',['../class_cpp_trader_1_1_order_book.html#a7598b6635840c321e4a3355065a0bfd2',1,'CppTrader::OrderBook']]],
+  ['bids',['bids',['../class_cpp_trader_1_1_order_book.html#afbf04d98ebcc6b69f7cee115c522047d',1,'CppTrader::OrderBook']]]
 ];
