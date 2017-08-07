43d41fa8081881f3cf17625c48ac7127335dbeb6
AppVeyor <team@appveyor.com>
2017-08-06 20:04:44 -0700

    Update generated documentation

--- search/all_1.js
+++ search/all_1.js
@@ -1,5 +1,7 @@
 var searchData=
 [
+  ['best_5fask',['best_ask',['../class_cpp_trader_1_1_order_book.html#aa0b7847d5d53f5adf938fcdc02b312d6',1,'CppTrader::OrderBook']]],
+  ['best_5fbid',['best_bid',['../class_cpp_trader_1_1_order_book.html#a7598b6635840c321e4a3355065a0bfd2',1,'CppTrader::OrderBook']]],
   ['bids',['bids',['../class_cpp_trader_1_1_order_book.html#afbf04d98ebcc6b69f7cee115c522047d',1,'CppTrader::OrderBook']]],
   ['breachedlevel',['BreachedLevel',['../struct_cpp_trader_1_1_i_t_c_h_1_1_m_w_c_b_status_message.html#a6a35fed90db2baae1b457ea28aafe196',1,'CppTrader::ITCH::MWCBStatusMessage']]],
   ['brokentrademessage',['BrokenTradeMessage',['../struct_cpp_trader_1_1_i_t_c_h_1_1_broken_trade_message.html',1,'CppTrader::ITCH']]],
--- search/functions_1.js
+++ search/functions_1.js
@@ -1,4 +1,6 @@
 var searchData=
 [
+  ['best_5fask',['best_ask',['../class_cpp_trader_1_1_order_book.html#aa0b7847d5d53f5adf938fcdc02b312d6',1,'CppTrader::OrderBook']]],
+  ['best_5fbid',['best_bid',['../class_cpp_trader_1_1_order_book.html#a7598b6635840c321e4a3355065a0bfd2',1,'CppTrader::OrderBook']]],
   ['bids',['bids',['../class_cpp_trader_1_1_order_book.html#afbf04d98ebcc6b69f7cee115c522047d',1,'CppTrader::OrderBook']]]
 ];
