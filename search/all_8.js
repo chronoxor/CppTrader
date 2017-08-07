25affe2ee373be0713bcd471936736e8a6f1df26
Travis CI <travis@travis-ci.org>
2017-08-07 10:20:13 +0000

    Update generated documentation

--- search/all_8.js
+++ search/all_8.js
@@ -1,6 +1,7 @@
 var searchData=
 [
-  ['level',['Level',['../struct_cpp_trader_1_1_level.html',1,'CppTrader::Level'],['../struct_cpp_trader_1_1_order.html#aa8eae2b048ee3907852c6f4247c93d9c',1,'CppTrader::Order::Level()'],['../struct_cpp_trader_1_1_level.html#ad7f6f01a12b663400ff489cfe2002987',1,'CppTrader::Level::Level(uint64_t price) noexcept'],['../struct_cpp_trader_1_1_level.html#a837f1d8bb9e677f0b629d6f5d358dc27',1,'CppTrader::Level::Level(const Level &amp;) noexcept=default'],['../struct_cpp_trader_1_1_level.html#aa16fb2bc8b869c4eeb2102cf5c22521d',1,'CppTrader::Level::Level(Level &amp;&amp;) noexcept=default']]],
+  ['level',['Level',['../struct_cpp_trader_1_1_level.html',1,'CppTrader']]],
+  ['level',['Level',['../struct_cpp_trader_1_1_level.html#ad7f6f01a12b663400ff489cfe2002987',1,'CppTrader::Level::Level(uint64_t price) noexcept'],['../struct_cpp_trader_1_1_level.html#a837f1d8bb9e677f0b629d6f5d358dc27',1,'CppTrader::Level::Level(const Level &amp;) noexcept=default'],['../struct_cpp_trader_1_1_level.html#aa16fb2bc8b869c4eeb2102cf5c22521d',1,'CppTrader::Level::Level(Level &amp;&amp;) noexcept=default']]],
   ['level_2eh',['level.h',['../level_8h.html',1,'']]],
   ['level_2einl',['level.inl',['../level_8inl.html',1,'']]],
   ['level1',['Level1',['../struct_cpp_trader_1_1_i_t_c_h_1_1_m_w_c_b_decline_message.html#ae95ec7967637258117392bcaba7ebb2a',1,'CppTrader::ITCH::MWCBDeclineMessage']]],
--- search/variables_6.js
+++ search/variables_6.js
@@ -1,6 +1,5 @@
 var searchData=
 [
-  ['level',['Level',['../struct_cpp_trader_1_1_order.html#aa8eae2b048ee3907852c6f4247c93d9c',1,'CppTrader::Order']]],
   ['level1',['Level1',['../struct_cpp_trader_1_1_i_t_c_h_1_1_m_w_c_b_decline_message.html#ae95ec7967637258117392bcaba7ebb2a',1,'CppTrader::ITCH::MWCBDeclineMessage']]],
   ['level2',['Level2',['../struct_cpp_trader_1_1_i_t_c_h_1_1_m_w_c_b_decline_message.html#a6b63fad1fd60010b6a558834bf0c6441',1,'CppTrader::ITCH::MWCBDeclineMessage']]],
   ['level3',['Level3',['../struct_cpp_trader_1_1_i_t_c_h_1_1_m_w_c_b_decline_message.html#a361107c596dc3d7a331360e36cb74e11',1,'CppTrader::ITCH::MWCBDeclineMessage']]],
