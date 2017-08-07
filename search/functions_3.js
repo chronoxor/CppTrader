3438df67d74ffdd08027b2ff5231bc0a4b582c99
AppVeyor <team@appveyor.com>
2017-08-06 20:20:00 -0700

    Update generated documentation

--- search/functions_3.js
+++ search/functions_3.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['empty',['empty',['../class_cpp_trader_1_1_order_book.html#afcdf58287f740c5dc6564f46db9273e9',1,'CppTrader::OrderBook']]],
+  ['empty',['empty',['../class_cpp_trader_1_1_order_book.html#af29b9ffa449928ad2464b18730b17c65',1,'CppTrader::OrderBook']]],
   ['executeorder',['ExecuteOrder',['../class_cpp_trader_1_1_market_manager.html#ad32f70313ea4ef2215abc1dc4c7214f7',1,'CppTrader::MarketManager::ExecuteOrder(uint64_t id, uint64_t quantity)'],['../class_cpp_trader_1_1_market_manager.html#aa463b90a17bdb750d38d3d9d7de59b76',1,'CppTrader::MarketManager::ExecuteOrder(uint64_t id, uint64_t price, uint64_t quantity)']]]
 ];
--- search/functions_c.js
+++ search/functions_c.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['size',['size',['../class_cpp_trader_1_1_order_book.html#a084e27c0baae36786308c019a2dbe91f',1,'CppTrader::OrderBook']]],
-  ['symbol',['symbol',['../class_cpp_trader_1_1_order_book.html#a4690d5f51c704dc5d9ad59838c578802',1,'CppTrader::OrderBook::symbol()'],['../struct_cpp_trader_1_1_symbol.html#a015b2810b4fc614a2fdad47292072093',1,'CppTrader::Symbol::Symbol(uint32_t id, const char name[8]) noexcept'],['../struct_cpp_trader_1_1_symbol.html#a025c627cc0052244f1b280fae41323ef',1,'CppTrader::Symbol::Symbol(const Symbol &amp;) noexcept=default'],['../struct_cpp_trader_1_1_symbol.html#ac7b7780281cab43367e3f8963417f5ca',1,'CppTrader::Symbol::Symbol(Symbol &amp;&amp;) noexcept=default']]]
+  ['size',['size',['../class_cpp_trader_1_1_order_book.html#a13da44c8bcc7fa1435df697e822eada4',1,'CppTrader::OrderBook']]],
+  ['symbol',['symbol',['../class_cpp_trader_1_1_order_book.html#a40c6fe1e71d3ecd61563cd3e1f6fa30f',1,'CppTrader::OrderBook::symbol()'],['../struct_cpp_trader_1_1_symbol.html#a015b2810b4fc614a2fdad47292072093',1,'CppTrader::Symbol::Symbol(uint32_t id, const char name[8]) noexcept'],['../struct_cpp_trader_1_1_symbol.html#a025c627cc0052244f1b280fae41323ef',1,'CppTrader::Symbol::Symbol(const Symbol &amp;) noexcept=default'],['../struct_cpp_trader_1_1_symbol.html#ac7b7780281cab43367e3f8963417f5ca',1,'CppTrader::Symbol::Symbol(Symbol &amp;&amp;) noexcept=default']]]
 ];
