39e8e002d0b467093e99beece81a7d073fec82fb
Travis CI <travis@travis-ci.org>
2017-08-10 11:46:18 +0000

    Update generated documentation

--- search/functions_0.js
+++ search/functions_0.js
@@ -3,5 +3,5 @@ var searchData=
   ['addorder',['AddOrder',['../class_cpp_trader_1_1_market_manager.html#a2e42352f0b517d43b773c666dd8dad46',1,'CppTrader::MarketManager']]],
   ['addorderbook',['AddOrderBook',['../class_cpp_trader_1_1_market_manager.html#a702434557d8436626f137b6ba7c47c2a',1,'CppTrader::MarketManager']]],
   ['addsymbol',['AddSymbol',['../class_cpp_trader_1_1_market_manager.html#a3aa11a489c9d3687ea9f5202e5edfa53',1,'CppTrader::MarketManager']]],
-  ['asks',['asks',['../class_cpp_trader_1_1_order_book.html#a487eab6ddb7c3351584a20e06fee903f',1,'CppTrader::OrderBook']]]
+  ['asks',['asks',['../class_cpp_trader_1_1_order_book.html#a4dcd025c487a8021350d7f245e8039da',1,'CppTrader::OrderBook']]]
 ];
