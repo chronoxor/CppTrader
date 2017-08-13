3c802879fad7ce3afdc30a37982bff355ac04c57
AppVeyor <team@appveyor.com>
2017-08-13 13:47:44 -0700

    Update generated documentation

--- search/functions_5.js
+++ search/functions_5.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['getorder',['GetOrder',['../class_cpp_trader_1_1_market_manager.html#a548467352a169872d0aeafabedb96d87',1,'CppTrader::MarketManager']]],
-  ['getorderbook',['GetOrderBook',['../class_cpp_trader_1_1_market_manager.html#a3fe6e64b0bd2ba54356a7fd8d7350346',1,'CppTrader::MarketManager']]],
-  ['getsymbol',['GetSymbol',['../class_cpp_trader_1_1_market_manager.html#ac6a48aa9f8a205825256738145a6b2bf',1,'CppTrader::MarketManager']]]
+  ['getorder',['GetOrder',['../class_cpp_trader_1_1_market_manager.html#a7ce60bf34f5d2e804bebd7f1d2306f39',1,'CppTrader::MarketManager']]],
+  ['getorderbook',['GetOrderBook',['../class_cpp_trader_1_1_market_manager.html#a4bb00fcee51645ba22646e47757cd6dc',1,'CppTrader::MarketManager']]],
+  ['getsymbol',['GetSymbol',['../class_cpp_trader_1_1_market_manager.html#ad30e2369f775c79d1c721c2e0613d5ca',1,'CppTrader::MarketManager']]]
 ];
