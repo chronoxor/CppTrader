875cc3fb02c96c7b2d3abab35e6790180a9fd2b9
AppVeyor <team@appveyor.com>
2017-08-15 16:26:57 -0700

    Update generated documentation

--- search/all_0.js
+++ search/all_0.js
@@ -7,7 +7,7 @@ var searchData=
   ['addordermpidmessage',['AddOrderMPIDMessage',['../struct_cpp_trader_1_1_i_t_c_h_1_1_add_order_m_p_i_d_message.html',1,'CppTrader::ITCH']]],
   ['addsymbol',['AddSymbol',['../class_cpp_trader_1_1_matching_1_1_market_manager.html#a8af3c694f482b9ee43e641a2981afccd',1,'CppTrader::Matching::MarketManager']]],
   ['ask',['ASK',['../namespace_cpp_trader_1_1_matching.html#a08cb139d374fe621043cc84ad62790aca9fb730caf1b2d638a1dcf747f0d4207d',1,'CppTrader::Matching']]],
-  ['asks',['asks',['../class_cpp_trader_1_1_matching_1_1_order_book.html#a5dc10173d986f48d519c5da0e4d8d29b',1,'CppTrader::Matching::OrderBook']]],
+  ['asks',['asks',['../class_cpp_trader_1_1_matching_1_1_order_book.html#a1d5f253f54d3647b01d7a2f24248c098',1,'CppTrader::Matching::OrderBook']]],
   ['attribution',['Attribution',['../struct_cpp_trader_1_1_i_t_c_h_1_1_add_order_m_p_i_d_message.html#a854289a0cf6e1d6c08096325273b6b68',1,'CppTrader::ITCH::AddOrderMPIDMessage']]],
   ['authenticity',['Authenticity',['../struct_cpp_trader_1_1_i_t_c_h_1_1_stock_directory_message.html#a4680743fa2e82a1b62aa10a75a3c1f8d',1,'CppTrader::ITCH::StockDirectoryMessage']]]
 ];
--- search/functions_6.js
+++ search/functions_6.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['ismatchingenabled',['IsMatchingEnabled',['../class_cpp_trader_1_1_matching_1_1_market_manager.html#a29805141155fa8746fe530564b1fee24',1,'CppTrader::Matching::MarketManager']]],
+  ['ismatchingenabled',['IsMatchingEnabled',['../class_cpp_trader_1_1_matching_1_1_market_manager.html#a4e89435ff1e735ae3fd30a1d7996db0e',1,'CppTrader::Matching::MarketManager']]],
   ['itchhandler',['ITCHHandler',['../class_cpp_trader_1_1_i_t_c_h_1_1_i_t_c_h_handler.html#abcf8e0c8fab2fe8f9c90cb4db5b0318d',1,'CppTrader::ITCH::ITCHHandler::ITCHHandler()'],['../class_cpp_trader_1_1_i_t_c_h_1_1_i_t_c_h_handler.html#aa53fd288010e9c630b605d04edbde80d',1,'CppTrader::ITCH::ITCHHandler::ITCHHandler(const ITCHHandler &amp;)=delete'],['../class_cpp_trader_1_1_i_t_c_h_1_1_i_t_c_h_handler.html#a859f0f2efd273d0575f983bdc0abc987',1,'CppTrader::ITCH::ITCHHandler::ITCHHandler(ITCHHandler &amp;&amp;) noexcept=default']]]
 ];
