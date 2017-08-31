06f77806cff6c58d66a15d4058678ac1457dfaf8
Travis CI <travis@travis-ci.org>
2017-08-31 00:02:15 +0000

    Update generated documentation

--- search/all_7.js
+++ search/all_7.js
@@ -10,11 +10,20 @@ var searchData=
   ['ipoquotingmessage',['IPOQuotingMessage',['../struct_cpp_trader_1_1_i_t_c_h_1_1_i_p_o_quoting_message.html',1,'CppTrader::ITCH']]],
   ['iporeleasequalifier',['IPOReleaseQualifier',['../struct_cpp_trader_1_1_i_t_c_h_1_1_i_p_o_quoting_message.html#a52866ecfca3eefa63e6a13cb299e3700',1,'CppTrader::ITCH::IPOQuotingMessage']]],
   ['iporeleasetime',['IPOReleaseTime',['../struct_cpp_trader_1_1_i_t_c_h_1_1_i_p_o_quoting_message.html#a8951e20ae828e5ddbaf3fcf433266934',1,'CppTrader::ITCH::IPOQuotingMessage']]],
+  ['isaon',['IsAON',['../struct_cpp_trader_1_1_matching_1_1_order.html#a30dea10e2ad1b54efc0173c9717cf1ec',1,'CppTrader::Matching::Order']]],
+  ['isbuy',['IsBuy',['../struct_cpp_trader_1_1_matching_1_1_order.html#a6aa7cc4356e15d2d6dbf1c9061a13ea0',1,'CppTrader::Matching::Order']]],
+  ['islimit',['IsLimit',['../struct_cpp_trader_1_1_matching_1_1_order.html#a8c957c7615be4455baf779bc6fd75d8f',1,'CppTrader::Matching::Order']]],
+  ['ismarket',['IsMarket',['../struct_cpp_trader_1_1_matching_1_1_order.html#aaa9cae97e88133e91342eeb984afa905',1,'CppTrader::Matching::Order']]],
   ['ismatchingenabled',['IsMatchingEnabled',['../class_cpp_trader_1_1_matching_1_1_market_manager.html#a29805141155fa8746fe530564b1fee24',1,'CppTrader::Matching::MarketManager']]],
+  ['issell',['IsSell',['../struct_cpp_trader_1_1_matching_1_1_order.html#aee5a720a45c09b6303266f9c8e720288',1,'CppTrader::Matching::Order']]],
+  ['isslippage',['IsSlippage',['../struct_cpp_trader_1_1_matching_1_1_order.html#a51cc41350e9b75a79db52b6437f3a850',1,'CppTrader::Matching::Order']]],
+  ['isstop',['IsStop',['../struct_cpp_trader_1_1_matching_1_1_order.html#ad6974b6a1184d3b6d1986dbdf5784bd6',1,'CppTrader::Matching::Order']]],
+  ['isstoplimit',['IsStopLimit',['../struct_cpp_trader_1_1_matching_1_1_order.html#a0dfe47bcc858391edf97199f168c3e6e',1,'CppTrader::Matching::Order']]],
   ['issueclassification',['IssueClassification',['../struct_cpp_trader_1_1_i_t_c_h_1_1_stock_directory_message.html#a7fe3fcce406cdb6cbf65e556a7eb2360',1,'CppTrader::ITCH::StockDirectoryMessage']]],
   ['issuesubtype',['IssueSubType',['../struct_cpp_trader_1_1_i_t_c_h_1_1_stock_directory_message.html#a759c2ed5b1947755466534da03582268',1,'CppTrader::ITCH::StockDirectoryMessage']]],
   ['itch_5fhandler_2ecpp',['itch_handler.cpp',['../itch__handler_8cpp.html',1,'']]],
   ['itch_5fhandler_2eh',['itch_handler.h',['../itch__handler_8h.html',1,'']]],
   ['itch_5fhandler_2einl',['itch_handler.inl',['../itch__handler_8inl.html',1,'']]],
-  ['itchhandler',['ITCHHandler',['../class_cpp_trader_1_1_i_t_c_h_1_1_i_t_c_h_handler.html',1,'CppTrader::ITCH::ITCHHandler'],['../class_cpp_trader_1_1_i_t_c_h_1_1_i_t_c_h_handler.html#abcf8e0c8fab2fe8f9c90cb4db5b0318d',1,'CppTrader::ITCH::ITCHHandler::ITCHHandler()'],['../class_cpp_trader_1_1_i_t_c_h_1_1_i_t_c_h_handler.html#aa53fd288010e9c630b605d04edbde80d',1,'CppTrader::ITCH::ITCHHandler::ITCHHandler(const ITCHHandler &amp;)=delete'],['../class_cpp_trader_1_1_i_t_c_h_1_1_i_t_c_h_handler.html#a859f0f2efd273d0575f983bdc0abc987',1,'CppTrader::ITCH::ITCHHandler::ITCHHandler(ITCHHandler &amp;&amp;) noexcept=default']]]
+  ['itchhandler',['ITCHHandler',['../class_cpp_trader_1_1_i_t_c_h_1_1_i_t_c_h_handler.html#abcf8e0c8fab2fe8f9c90cb4db5b0318d',1,'CppTrader::ITCH::ITCHHandler::ITCHHandler()'],['../class_cpp_trader_1_1_i_t_c_h_1_1_i_t_c_h_handler.html#aa53fd288010e9c630b605d04edbde80d',1,'CppTrader::ITCH::ITCHHandler::ITCHHandler(const ITCHHandler &amp;)=delete'],['../class_cpp_trader_1_1_i_t_c_h_1_1_i_t_c_h_handler.html#a859f0f2efd273d0575f983bdc0abc987',1,'CppTrader::ITCH::ITCHHandler::ITCHHandler(ITCHHandler &amp;&amp;) noexcept=default']]],
+  ['itchhandler',['ITCHHandler',['../class_cpp_trader_1_1_i_t_c_h_1_1_i_t_c_h_handler.html',1,'CppTrader::ITCH']]]
 ];
--- search/enumvalues_7.js
+++ search/enumvalues_7.js
@@ -1,6 +1,8 @@
 var searchData=
 [
   ['sell',['SELL',['../namespace_cpp_trader_1_1_matching.html#a07e8bfcb84f50475ba6ca9d29a6ca668a860926e6fc3f191e18ee7a98259164b8',1,'CppTrader::Matching']]],
+  ['stop',['STOP',['../namespace_cpp_trader_1_1_matching.html#a3f665b3bd035eb7a647258ce0ca1dd43a615a46af313786fc4e349f34118be111',1,'CppTrader::Matching']]],
+  ['stoplimit',['STOPLIMIT',['../namespace_cpp_trader_1_1_matching.html#a3f665b3bd035eb7a647258ce0ca1dd43a18ebe4d0525292b23ab1fe0bda4b19af',1,'CppTrader::Matching']]],
   ['symbol_5fduplicate',['SYMBOL_DUPLICATE',['../namespace_cpp_trader_1_1_matching.html#a1ec2fbd049c2332c538badf16c46b86ea6541d38b7ecc236bdf19bfd60706322c',1,'CppTrader::Matching']]],
   ['symbol_5fnot_5ffound',['SYMBOL_NOT_FOUND',['../namespace_cpp_trader_1_1_matching.html#a1ec2fbd049c2332c538badf16c46b86ea7e154a753690fd4e3fe5d08566431cb0',1,'CppTrader::Matching']]]
 ];
