ff684005c5c5033025fb9e10b646939927bdcca7
Travis CI <travis@travis-ci.org>
2017-09-05 00:06:17 +0000

    Update generated documentation

--- search/all_8.js
+++ search/all_8.js
@@ -32,6 +32,5 @@ var searchData=
   ['itch_5fhandler_2ecpp',['itch_handler.cpp',['../itch__handler_8cpp.html',1,'']]],
   ['itch_5fhandler_2eh',['itch_handler.h',['../itch__handler_8h.html',1,'']]],
   ['itch_5fhandler_2einl',['itch_handler.inl',['../itch__handler_8inl.html',1,'']]],
-  ['itchhandler',['ITCHHandler',['../class_cpp_trader_1_1_i_t_c_h_1_1_i_t_c_h_handler.html',1,'CppTrader::ITCH']]],
-  ['itchhandler',['ITCHHandler',['../class_cpp_trader_1_1_i_t_c_h_1_1_i_t_c_h_handler.html#abcf8e0c8fab2fe8f9c90cb4db5b0318d',1,'CppTrader::ITCH::ITCHHandler::ITCHHandler()'],['../class_cpp_trader_1_1_i_t_c_h_1_1_i_t_c_h_handler.html#aa53fd288010e9c630b605d04edbde80d',1,'CppTrader::ITCH::ITCHHandler::ITCHHandler(const ITCHHandler &amp;)=delete'],['../class_cpp_trader_1_1_i_t_c_h_1_1_i_t_c_h_handler.html#a859f0f2efd273d0575f983bdc0abc987',1,'CppTrader::ITCH::ITCHHandler::ITCHHandler(ITCHHandler &amp;&amp;) noexcept=default']]]
+  ['itchhandler',['ITCHHandler',['../class_cpp_trader_1_1_i_t_c_h_1_1_i_t_c_h_handler.html',1,'CppTrader::ITCH::ITCHHandler'],['../class_cpp_trader_1_1_i_t_c_h_1_1_i_t_c_h_handler.html#abcf8e0c8fab2fe8f9c90cb4db5b0318d',1,'CppTrader::ITCH::ITCHHandler::ITCHHandler()'],['../class_cpp_trader_1_1_i_t_c_h_1_1_i_t_c_h_handler.html#aa53fd288010e9c630b605d04edbde80d',1,'CppTrader::ITCH::ITCHHandler::ITCHHandler(const ITCHHandler &amp;)=delete'],['../class_cpp_trader_1_1_i_t_c_h_1_1_i_t_c_h_handler.html#a859f0f2efd273d0575f983bdc0abc987',1,'CppTrader::ITCH::ITCHHandler::ITCHHandler(ITCHHandler &amp;&amp;) noexcept=default']]]
 ];
