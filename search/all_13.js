375d626fb4ff7335601afe0a9f06fcb1c6d2f393
Travis CI <travis@travis-ci.org>
2017-09-06 20:37:17 +0000

    Update generated documentation

--- search/all_13.js
+++ search/all_13.js
@@ -1,8 +1,8 @@
 var searchData=
 [
-  ['validate',['Validate',['../struct_cpp_trader_1_1_matching_1_1_order.html#a33e74558ea25cfd97969adc7ce6bd443',1,'CppTrader::Matching::Order']]],
+  ['validate',['Validate',['../struct_cpp_trader_1_1_matching_1_1_order.html#a0c7b2a715d97c6c649c0ffec659a3794',1,'CppTrader::Matching::Order']]],
   ['version',['version',['../namespace_cpp_trader.html#ae8e27556b85c591333c48f5b8cd0aeea',1,'CppTrader']]],
   ['version_2eh',['version.h',['../version_8h.html',1,'']]],
-  ['visiblequantity',['VisibleQuantity',['../struct_cpp_trader_1_1_matching_1_1_order.html#aace14c2fef34c9c9df9ff184faeabe70',1,'CppTrader::Matching::Order']]],
+  ['visiblequantity',['VisibleQuantity',['../struct_cpp_trader_1_1_matching_1_1_order.html#a07b939303653e9135a9c9fb31dd3e9c8',1,'CppTrader::Matching::Order']]],
   ['visiblevolume',['VisibleVolume',['../struct_cpp_trader_1_1_matching_1_1_level.html#ad1c218df0a7f0b1cee224a23e0d07bed',1,'CppTrader::Matching::Level']]]
 ];
