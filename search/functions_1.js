5ef11f3a8484093e888a7e3a021cd81a1dfc4f1b
AppVeyor <team@appveyor.com>
2017-09-06 09:24:30 -0700

    Update generated documentation

--- search/functions_1.js
+++ search/functions_1.js
@@ -1,11 +1,11 @@
 var searchData=
 [
-  ['best_5fask',['best_ask',['../class_cpp_trader_1_1_matching_1_1_order_book.html#a21ec500866c23c7eecda0dac7b96c3dd',1,'CppTrader::Matching::OrderBook']]],
-  ['best_5fbid',['best_bid',['../class_cpp_trader_1_1_matching_1_1_order_book.html#a3710230efc0aec45583987de3dd36d48',1,'CppTrader::Matching::OrderBook']]],
-  ['best_5fbuy_5fstop',['best_buy_stop',['../class_cpp_trader_1_1_matching_1_1_order_book.html#a26a666955ad455639054408ba5217728',1,'CppTrader::Matching::OrderBook']]],
-  ['best_5fsell_5fstop',['best_sell_stop',['../class_cpp_trader_1_1_matching_1_1_order_book.html#aafbc097582b78ce2ce42b58620a95a0f',1,'CppTrader::Matching::OrderBook']]],
-  ['bids',['bids',['../class_cpp_trader_1_1_matching_1_1_order_book.html#a613c21ef0c5097641a559032b0d1df3d',1,'CppTrader::Matching::OrderBook']]],
-  ['buy_5fstop',['buy_stop',['../class_cpp_trader_1_1_matching_1_1_order_book.html#a9eb5e4902ca35a2d5eab3d8fa399ad44',1,'CppTrader::Matching::OrderBook']]],
+  ['best_5fask',['best_ask',['../class_cpp_trader_1_1_matching_1_1_order_book.html#abf77960bd0496b054d16d8124bb5d697',1,'CppTrader::Matching::OrderBook']]],
+  ['best_5fbid',['best_bid',['../class_cpp_trader_1_1_matching_1_1_order_book.html#ae9030bcb39071b837fff907119e974af',1,'CppTrader::Matching::OrderBook']]],
+  ['best_5fbuy_5fstop',['best_buy_stop',['../class_cpp_trader_1_1_matching_1_1_order_book.html#a73d80479a324347027b74ee3dd723dba',1,'CppTrader::Matching::OrderBook']]],
+  ['best_5fsell_5fstop',['best_sell_stop',['../class_cpp_trader_1_1_matching_1_1_order_book.html#ad20f7301333339951d0d04a059cce1b6',1,'CppTrader::Matching::OrderBook']]],
+  ['bids',['bids',['../class_cpp_trader_1_1_matching_1_1_order_book.html#acce4c9e0581d030694c2622fdf5da11c',1,'CppTrader::Matching::OrderBook']]],
+  ['buy_5fstop',['buy_stop',['../class_cpp_trader_1_1_matching_1_1_order_book.html#a31a7a3c9ed0c8060f9ee2d1ba01866d7',1,'CppTrader::Matching::OrderBook']]],
   ['buylimit',['BuyLimit',['../struct_cpp_trader_1_1_matching_1_1_order.html#aa501f090069636a0b874c5cab1d10893',1,'CppTrader::Matching::Order']]],
   ['buymarket',['BuyMarket',['../struct_cpp_trader_1_1_matching_1_1_order.html#a25fb864940ef5a846e6a4ce7631d153a',1,'CppTrader::Matching::Order']]],
   ['buystop',['BuyStop',['../struct_cpp_trader_1_1_matching_1_1_order.html#ae2e791988c4ff5127edcde51afbe7c98',1,'CppTrader::Matching::Order']]],
--- search/functions_e.js
+++ search/functions_e.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['validate',['Validate',['../struct_cpp_trader_1_1_matching_1_1_order.html#a0c7b2a715d97c6c649c0ffec659a3794',1,'CppTrader::Matching::Order']]],
-  ['visiblequantity',['VisibleQuantity',['../struct_cpp_trader_1_1_matching_1_1_order.html#a07b939303653e9135a9c9fb31dd3e9c8',1,'CppTrader::Matching::Order']]]
+  ['validate',['Validate',['../struct_cpp_trader_1_1_matching_1_1_order.html#a33e74558ea25cfd97969adc7ce6bd443',1,'CppTrader::Matching::Order']]],
+  ['visiblequantity',['VisibleQuantity',['../struct_cpp_trader_1_1_matching_1_1_order.html#aace14c2fef34c9c9df9ff184faeabe70',1,'CppTrader::Matching::Order']]]
 ];
