a57a525fb99599ae9429d4d52490a26f6b090553
AppVeyor <team@appveyor.com>
2018-02-07 06:16:32 -0800

    Update generated documentation

--- search/functions_d.js
+++ search/functions_d.js
@@ -1,13 +1,13 @@
 var searchData=
 [
-  ['sell_5fstop',['sell_stop',['../class_cpp_trader_1_1_matching_1_1_order_book.html#aae9573477cc3b606861879761f189b63',1,'CppTrader::Matching::OrderBook']]],
+  ['sell_5fstop',['sell_stop',['../class_cpp_trader_1_1_matching_1_1_order_book.html#a0c6d2d1f9a24e69353511099f238a96d',1,'CppTrader::Matching::OrderBook']]],
   ['selllimit',['SellLimit',['../struct_cpp_trader_1_1_matching_1_1_order.html#a18031643b63fa885dae09c7ee8144c68',1,'CppTrader::Matching::Order']]],
   ['sellmarket',['SellMarket',['../struct_cpp_trader_1_1_matching_1_1_order.html#a96667676a7f9f5ffe6e65c1d49f0fe8f',1,'CppTrader::Matching::Order']]],
   ['sellstop',['SellStop',['../struct_cpp_trader_1_1_matching_1_1_order.html#aa073c66ce90d36428a9832ec566625fb',1,'CppTrader::Matching::Order']]],
   ['sellstoplimit',['SellStopLimit',['../struct_cpp_trader_1_1_matching_1_1_order.html#a61a046857b3daac9cdd836a3c39b9234',1,'CppTrader::Matching::Order']]],
-  ['size',['size',['../class_cpp_trader_1_1_matching_1_1_order_book.html#a6cf7f3856184478471ffad543b386b67',1,'CppTrader::Matching::OrderBook']]],
+  ['size',['size',['../class_cpp_trader_1_1_matching_1_1_order_book.html#a7bd161a6132290ce88e1395ca58afa7e',1,'CppTrader::Matching::OrderBook']]],
   ['stop',['Stop',['../struct_cpp_trader_1_1_matching_1_1_order.html#a2cec088123bef9a5f6af082b7d66b239',1,'CppTrader::Matching::Order']]],
   ['stoplimit',['StopLimit',['../struct_cpp_trader_1_1_matching_1_1_order.html#a6efe2eff88314fd37190e7f28d55a5e5',1,'CppTrader::Matching::Order']]],
-  ['symbol',['symbol',['../class_cpp_trader_1_1_matching_1_1_order_book.html#aa97a9382fa85bd99a3978577634b68cc',1,'CppTrader::Matching::OrderBook::symbol()'],['../struct_cpp_trader_1_1_matching_1_1_symbol.html#a1fe0874e52e6b18af045410f068eb319',1,'CppTrader::Matching::Symbol::Symbol() noexcept=default'],['../struct_cpp_trader_1_1_matching_1_1_symbol.html#ab377f804b856f70a9b07bd524f48253b',1,'CppTrader::Matching::Symbol::Symbol(uint32_t id, const char name[8]) noexcept'],['../struct_cpp_trader_1_1_matching_1_1_symbol.html#a193f54117d133c0c28f8c3353385058d',1,'CppTrader::Matching::Symbol::Symbol(const Symbol &amp;) noexcept=default'],['../struct_cpp_trader_1_1_matching_1_1_symbol.html#a9a082f48b11b3fae3464d87bf31130e4',1,'CppTrader::Matching::Symbol::Symbol(Symbol &amp;&amp;) noexcept=default']]],
-  ['symbols',['symbols',['../class_cpp_trader_1_1_matching_1_1_market_manager.html#a68f3456c803eae7221f21c081ab7af88',1,'CppTrader::Matching::MarketManager']]]
+  ['symbol',['symbol',['../class_cpp_trader_1_1_matching_1_1_order_book.html#a28178db0c73fff8c1ce31ab0a9b40441',1,'CppTrader::Matching::OrderBook::symbol()'],['../struct_cpp_trader_1_1_matching_1_1_symbol.html#a1fe0874e52e6b18af045410f068eb319',1,'CppTrader::Matching::Symbol::Symbol() noexcept=default'],['../struct_cpp_trader_1_1_matching_1_1_symbol.html#ab377f804b856f70a9b07bd524f48253b',1,'CppTrader::Matching::Symbol::Symbol(uint32_t id, const char name[8]) noexcept'],['../struct_cpp_trader_1_1_matching_1_1_symbol.html#a193f54117d133c0c28f8c3353385058d',1,'CppTrader::Matching::Symbol::Symbol(const Symbol &amp;) noexcept=default'],['../struct_cpp_trader_1_1_matching_1_1_symbol.html#a9a082f48b11b3fae3464d87bf31130e4',1,'CppTrader::Matching::Symbol::Symbol(Symbol &amp;&amp;) noexcept=default']]],
+  ['symbols',['symbols',['../class_cpp_trader_1_1_matching_1_1_market_manager.html#aaf9c2c2e0feb927fc27e5bbae0432b77',1,'CppTrader::Matching::MarketManager']]]
 ];
