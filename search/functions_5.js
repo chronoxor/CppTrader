2ee39731f2cc115f20e1b3bf54408cba140b6ba9
AppVeyor <team@appveyor.com>
2018-04-27 22:04:02 -0700

    Update generated documentation

--- search/functions_5.js
+++ search/functions_5.js
@@ -1,12 +1,12 @@
 var searchData=
 [
-  ['getask',['GetAsk',['../class_cpp_trader_1_1_matching_1_1_order_book.html#a2cfdda384fa19b3c8411541ce14ffa63',1,'CppTrader::Matching::OrderBook']]],
-  ['getbid',['GetBid',['../class_cpp_trader_1_1_matching_1_1_order_book.html#aa034c7378887b55f56ca131abb4121fb',1,'CppTrader::Matching::OrderBook']]],
-  ['getbuystoplevel',['GetBuyStopLevel',['../class_cpp_trader_1_1_matching_1_1_order_book.html#ac6675737c90bd4c45057e4bcb7d332fd',1,'CppTrader::Matching::OrderBook']]],
-  ['getorder',['GetOrder',['../class_cpp_trader_1_1_matching_1_1_market_manager.html#a91c60b7ed9e149e3d0bbfee78b1b0f5b',1,'CppTrader::Matching::MarketManager']]],
-  ['getorderbook',['GetOrderBook',['../class_cpp_trader_1_1_matching_1_1_market_manager.html#a2210ef75c65068fd248000143dfce4a9',1,'CppTrader::Matching::MarketManager']]],
-  ['getsellstoplevel',['GetSellStopLevel',['../class_cpp_trader_1_1_matching_1_1_order_book.html#aa2789b9ed082330c39cd191224951cec',1,'CppTrader::Matching::OrderBook']]],
-  ['getsymbol',['GetSymbol',['../class_cpp_trader_1_1_matching_1_1_market_manager.html#add7db176f8b6b23edd60b074174d9260',1,'CppTrader::Matching::MarketManager']]],
-  ['gettrailingbuystoplevel',['GetTrailingBuyStopLevel',['../class_cpp_trader_1_1_matching_1_1_order_book.html#a9b2a53bdf4fd4d8e8075ea0f8cef95c4',1,'CppTrader::Matching::OrderBook']]],
-  ['gettrailingsellstoplevel',['GetTrailingSellStopLevel',['../class_cpp_trader_1_1_matching_1_1_order_book.html#aaa88be9174acd70d1e848e152a791541',1,'CppTrader::Matching::OrderBook']]]
+  ['getask',['GetAsk',['../class_cpp_trader_1_1_matching_1_1_order_book.html#ad0dd5dab7703b8ddec14ebcd676dd6b4',1,'CppTrader::Matching::OrderBook']]],
+  ['getbid',['GetBid',['../class_cpp_trader_1_1_matching_1_1_order_book.html#a8109675f692299f1ad6fa1c070b564cc',1,'CppTrader::Matching::OrderBook']]],
+  ['getbuystoplevel',['GetBuyStopLevel',['../class_cpp_trader_1_1_matching_1_1_order_book.html#a893a70bc272f634fab2ea65b27918a47',1,'CppTrader::Matching::OrderBook']]],
+  ['getorder',['GetOrder',['../class_cpp_trader_1_1_matching_1_1_market_manager.html#aa2ed4acbee93b7a24a1ce248f9dcbae1',1,'CppTrader::Matching::MarketManager']]],
+  ['getorderbook',['GetOrderBook',['../class_cpp_trader_1_1_matching_1_1_market_manager.html#a52a1323a6e2be9be80411c3aebd66dcc',1,'CppTrader::Matching::MarketManager']]],
+  ['getsellstoplevel',['GetSellStopLevel',['../class_cpp_trader_1_1_matching_1_1_order_book.html#ad8841b978371a9c9752b940274aace35',1,'CppTrader::Matching::OrderBook']]],
+  ['getsymbol',['GetSymbol',['../class_cpp_trader_1_1_matching_1_1_market_manager.html#a3767297a3811c0341aff8dc97a5e6e29',1,'CppTrader::Matching::MarketManager']]],
+  ['gettrailingbuystoplevel',['GetTrailingBuyStopLevel',['../class_cpp_trader_1_1_matching_1_1_order_book.html#ad50372bc5271dd7809c804fc5bf89687',1,'CppTrader::Matching::OrderBook']]],
+  ['gettrailingsellstoplevel',['GetTrailingSellStopLevel',['../class_cpp_trader_1_1_matching_1_1_order_book.html#a5e2eb1df68bddf826ae649e25ea6a24c',1,'CppTrader::Matching::OrderBook']]]
 ];
