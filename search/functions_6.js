3d209f14eea9e21ec476c1758855c73d2c1a9c9c
AppVeyor <team@appveyor.com>
2018-05-30 19:12:36 -0700

    Update generated documentation

--- search/functions_6.js
+++ search/functions_6.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['hiddenquantity',['HiddenQuantity',['../struct_cpp_trader_1_1_matching_1_1_order.html#aa3e4861d7ecd323ee3304a0704922a60',1,'CppTrader::Matching::Order']]]
+  ['hiddenquantity',['HiddenQuantity',['../struct_cpp_trader_1_1_matching_1_1_order.html#a9261ec6373eede5b55f2ea83f270600a',1,'CppTrader::Matching::Order']]]
 ];
