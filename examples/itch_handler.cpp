9b914850585810303764582df7f106ad688efa9c
Ivan Shynkarenka <chronoxor@gmail.com>
2018-12-04 16:35:30 +0300

    Submodule Sync

--- examples/itch_handler.cpp
+++ examples/itch_handler.cpp
@@ -17,27 +17,27 @@ using namespace CppTrader::ITCH;
 class MyITCHHandler : public ITCHHandler
 {
 protected:
-    bool onMessage(const SystemEventMessage& message) override { return OutputMessage(message); }
+//    bool onMessage(const SystemEventMessage& message) override { return OutputMessage(message); }
     bool onMessage(const StockDirectoryMessage& message) override { return OutputMessage(message); }
-    bool onMessage(const StockTradingActionMessage& message) override { return OutputMessage(message); }
-    bool onMessage(const RegSHOMessage& message) override { return OutputMessage(message); }
-    bool onMessage(const MarketParticipantPositionMessage& message) override { return OutputMessage(message); }
-    bool onMessage(const MWCBDeclineMessage& message) override { return OutputMessage(message); }
-    bool onMessage(const MWCBStatusMessage& message) override { return OutputMessage(message); }
-    bool onMessage(const IPOQuotingMessage& message) override { return OutputMessage(message); }
-    bool onMessage(const AddOrderMessage& message) override { return OutputMessage(message); }
-    bool onMessage(const AddOrderMPIDMessage& message) override { return OutputMessage(message); }
-    bool onMessage(const OrderExecutedMessage& message) override { return OutputMessage(message); }
-    bool onMessage(const OrderExecutedWithPriceMessage& message) override { return OutputMessage(message); }
-    bool onMessage(const OrderCancelMessage& message) override { return OutputMessage(message); }
-    bool onMessage(const OrderDeleteMessage& message) override { return OutputMessage(message); }
-    bool onMessage(const OrderReplaceMessage& message) override { return OutputMessage(message); }
-    bool onMessage(const TradeMessage& message) override { return OutputMessage(message); }
-    bool onMessage(const CrossTradeMessage& message) override { return OutputMessage(message); }
-    bool onMessage(const BrokenTradeMessage& message) override { return OutputMessage(message); }
-    bool onMessage(const NOIIMessage& message) override { return OutputMessage(message); }
-    bool onMessage(const RPIIMessage& message) override { return OutputMessage(message); }
-    bool onMessage(const UnknownMessage& message) override { return OutputMessage(message); }
+ //   bool onMessage(const StockTradingActionMessage& message) override { return OutputMessage(message); }
+ //   bool onMessage(const RegSHOMessage& message) override { return OutputMessage(message); }
+ //   bool onMessage(const MarketParticipantPositionMessage& message) override { return OutputMessage(message); }
+ //   bool onMessage(const MWCBDeclineMessage& message) override { return OutputMessage(message); }
+ //   bool onMessage(const MWCBStatusMessage& message) override { return OutputMessage(message); }
+ //   bool onMessage(const IPOQuotingMessage& message) override { return OutputMessage(message); }
+ //   bool onMessage(const AddOrderMessage& message) override { return OutputMessage(message); }
+ //   bool onMessage(const AddOrderMPIDMessage& message) override { return OutputMessage(message); }
+ //   bool onMessage(const OrderExecutedMessage& message) override { return OutputMessage(message); }
+ //   bool onMessage(const OrderExecutedWithPriceMessage& message) override { return OutputMessage(message); }
+ //   bool onMessage(const OrderCancelMessage& message) override { return OutputMessage(message); }
+ //   bool onMessage(const OrderDeleteMessage& message) override { return OutputMessage(message); }
+ //   bool onMessage(const OrderReplaceMessage& message) override { return OutputMessage(message); }
+ //   bool onMessage(const TradeMessage& message) override { return OutputMessage(message); }
+ //   bool onMessage(const CrossTradeMessage& message) override { return OutputMessage(message); }
+ //   bool onMessage(const BrokenTradeMessage& message) override { return OutputMessage(message); }
+ //   bool onMessage(const NOIIMessage& message) override { return OutputMessage(message); }
+ //   bool onMessage(const RPIIMessage& message) override { return OutputMessage(message); }
+ //   bool onMessage(const UnknownMessage& message) override { return OutputMessage(message); }
 
 private:
     template <class TMessage>
